e2667bc08bba146115d1e4189d640f888b5662e2
Franco Román Meola <franco.meola@icloud.com>
2014-08-28 14:29:10 -0300

    Cambios varios en ListHotels (ej 2).

--- HotelApp/src/main/java/ar/edu/itba/it/paw/Hotel.java
+++ HotelApp/src/main/java/ar/edu/itba/it/paw/Hotel.java
@@ -1,19 +1,20 @@
 package ar.edu.itba.it.paw;
 
 public class Hotel {
-	
+
 	private int code;
 	private String name;
 	private String description;
-	
-	public Hotel(int code, String name, String description){
+
+	public Hotel(int code, String name, String description) {
 		this.code = code;
 		this.name = name;
 		this.description = description;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "<p>" + code + "::" + name + "::" + description + "</p>";
+		return "<tr><td>" + code + "</td><td>" + name + "</td><td>"
+				+ description + "</td>";
 	}
 }
--- HotelApp/src/main/java/ar/edu/itba/it/paw/ListHotels.java
+++ HotelApp/src/main/java/ar/edu/itba/it/paw/ListHotels.java
@@ -11,19 +11,37 @@ import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class ListHotels extends HttpServlet {
-	
+
 	private List<Hotel> l = new ArrayList<Hotel>();
-	
+
+	public ListHotels() {
+		initHotels();
+	}
+
+	/**
+	 * Inicializa la lista con los datos.
+	 */
+	private void initHotels() {
+		l.add(new Hotel(120, "NH Latino", "Un lugar como ninguno"));
+		l.add(new Hotel(300, "Hilton", "Somos vos"));
+		l.add(new Hotel(645, "Sheraton", "Serving People"));
+	}
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
-		l.add(new Hotel(120, "NH Hotel", "Texto"));
-		l.add(new Hotel(300, "Hilton", "Texto"));
-
-		resp.getWriter().append("<html><body>ListHotels!");
-		resp.getWriter().append(l.toString());
-		resp.getWriter().append("</body></html>");
+		resp.getWriter().append("<html>");
+		resp.getWriter().append("<head><title>Hoteles</title></head>");
+		resp.getWriter().append("<body>");
+		resp.getWriter().append("<h1>Hoteles</h1></br>");
+		resp.getWriter()
+				.append("<table><tr><td><b>Código</b></td><td><b>Nombre</b></td><td><b>Descripción</b></td>");
+		for (Hotel h : l) {
+			resp.getWriter().append(h.toString());
+		}
+		resp.getWriter().append("</table>");
+		resp.getWriter().append("</body>");
+		resp.getWriter().append("</html>");
 	}
 
 }
