31bc799833b54f71348b525d3068dc316fce8808
Franco Román Meola <franco.meola@icloud.com>
2014-08-31 10:44:29 -0300

    Terminado el ej2 guía 3. Se agregó el jsp de viewHotel.

--- HotelApp/src/main/java/ar/edu/itba/it/paw/ViewHotel.java
+++ HotelApp/src/main/java/ar/edu/itba/it/paw/ViewHotel.java
@@ -27,46 +27,57 @@ public class ViewHotel extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		currentHotelCode = req.getParameter("code");
-		
 		currentHotel = hm.getHotel(currentHotelCode);
 		HttpSession session = req.getSession();
-		
-		resp.getWriter().append("<html>");
-		resp.getWriter().append("<head>");
-		resp.getWriter().append("<title>Hotel " + currentHotel.getName() + "</title>");
-		resp.getWriter().append("<link rel=\"stylesheet\" href=\"//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css\">");
-		resp.getWriter().append("</head>");
-		resp.getWriter().append("<body><div class=\"container\">");
-		resp.getWriter().append("<h1>Hotel " + currentHotel.getName() + "</h1></br>");
-		resp.getWriter().append("<h2>" + currentHotel.getDescription() + "</h2>");
-		resp.getWriter().append("<h3>Código: " + currentHotel.getCode() + "</h3>");
-		resp.getWriter()
-				.append("<table class=\"table table-striped\"><tr><td><b>Usuario</b></td><td><b>EMail</b></td><td><b>Comentario</b></td>");
-		for(Comment c : currentHotel.getComments()) {
-			resp.getWriter().append(c.toString());
-		}
-		resp.getWriter().append("</table>");
-		resp.getWriter().append("</br>");
-		resp.getWriter().append("<h3>Deje su comentario aquí</h3>");
-		
-		resp.getWriter().append("<form role=\"form\" action=\"viewHotel\" method=\"post\"><div>");
-		resp.getWriter().append("<input type=\"hidden\" name=\"code\" value=\"" + currentHotelCode + "\">");
-		
-		if(session.getAttribute("name") != null){
-			resp.getWriter().append("<div class=\"form-group\"><label>" + session.getAttribute("name") + "</label></div>");
-			resp.getWriter().append("<div class=\"form-group\"><label>" + session.getAttribute("email") + "</label></div>");
-		} else {
-		resp.getWriter().append("<div><label>Usuario </label><input type=\"text\" name=\"usuario\" placeholder=\"Usuario\"></div>");
-		resp.getWriter().append("<div><label>EMail </label><input type=\"text\" name=\"email\" placeholder=\"EMail\"></div>");
-		}
-		
-		resp.getWriter().append("<div class=\"form-group\"><label>Comentario </label><textarea name=\"comentario\" class=\"form-control\" placeholder=\"Comentario\"></textarea></div>");
-		resp.getWriter().append("<button type=\"submit\" class=\"btn btn-primary\">Enviar</button>");
-		resp.getWriter().append("<button type=\"reset\" class=\"btn btn-default\">Limpiar</button>");
-		resp.getWriter().append("</div></form>");
-		resp.getWriter().append("</div></body>");
-		resp.getWriter().append("</html>");
+		req.setAttribute("hotel", currentHotel);
+		req.setAttribute("sesión", session);
+		req.getRequestDispatcher("/WEB-INF/jsp/viewHotel.jsp").forward(req, resp);
 	}
+	
+//	@Override
+//	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+//			throws ServletException, IOException {
+//		currentHotelCode = req.getParameter("code");
+//		
+//		currentHotel = hm.getHotel(currentHotelCode);
+//		HttpSession session = req.getSession();
+//		
+//		resp.getWriter().append("<html>");
+//		resp.getWriter().append("<head>");
+//		resp.getWriter().append("<title>Hotel " + currentHotel.getName() + "</title>");
+//		resp.getWriter().append("<link rel=\"stylesheet\" href=\"//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css\">");
+//		resp.getWriter().append("</head>");
+//		resp.getWriter().append("<body><div class=\"container\">");
+//		resp.getWriter().append("<h1>Hotel " + currentHotel.getName() + "</h1></br>");
+//		resp.getWriter().append("<h2>" + currentHotel.getDescription() + "</h2>");
+//		resp.getWriter().append("<h3>Código: " + currentHotel.getCode() + "</h3>");
+//		resp.getWriter()
+//				.append("<table class=\"table table-striped\"><tr><td><b>Usuario</b></td><td><b>EMail</b></td><td><b>Comentario</b></td>");
+//		for(Comment c : currentHotel.getComments()) {
+//			resp.getWriter().append(c.toString());
+//		}
+//		resp.getWriter().append("</table>");
+//		resp.getWriter().append("</br>");
+//		resp.getWriter().append("<h3>Deje su comentario aquí</h3>");
+//		
+//		resp.getWriter().append("<form role=\"form\" action=\"viewHotel\" method=\"post\"><div>");
+//		resp.getWriter().append("<input type=\"hidden\" name=\"code\" value=\"" + currentHotelCode + "\">");
+//		
+//		if(session.getAttribute("name") != null){
+//			resp.getWriter().append("<div class=\"form-group\"><label>" + session.getAttribute("name") + "</label></div>");
+//			resp.getWriter().append("<div class=\"form-group\"><label>" + session.getAttribute("email") + "</label></div>");
+//		} else {
+//		resp.getWriter().append("<div><label>Usuario </label><input type=\"text\" name=\"usuario\" placeholder=\"Usuario\"></div>");
+//		resp.getWriter().append("<div><label>EMail </label><input type=\"text\" name=\"email\" placeholder=\"EMail\"></div>");
+//		}
+//		
+//		resp.getWriter().append("<div class=\"form-group\"><label>Comentario </label><textarea name=\"comentario\" class=\"form-control\" placeholder=\"Comentario\"></textarea></div>");
+//		resp.getWriter().append("<button type=\"submit\" class=\"btn btn-primary\">Enviar</button>");
+//		resp.getWriter().append("<button type=\"reset\" class=\"btn btn-default\">Limpiar</button>");
+//		resp.getWriter().append("</div></form>");
+//		resp.getWriter().append("</div></body>");
+//		resp.getWriter().append("</html>");
+//	}
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
---
+++ HotelApp/src/main/webapp/WEB-INF/jsp/viewHotel.jsp
@@ -0,0 +1,42 @@
+<%@ page contentType="text/html" %>
+<%@ page pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<title><c:out value="Hotel ${hotel.name}"/></title>
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	</head>
+	<body>
+	<div class="container">
+		<h1><c:out value="Hotel ${hotel.name}"/></h1>
+		<h2><c:out value="${hotel.description}"/></h2>
+		<h3><c:out value="Codigo: ${hotel.code}"/></h3>
+		<table class="table table-striped">
+			<tr>
+				<td><b>Usuario</b></td>
+				<td><b>EMail</b></td>
+				<td><b>Comentario</b></td>
+			</tr>
+			<c:forEach var="c" items="${hotel.comments}">
+				<tr>
+					<td><c:out value="${c.username}"/></td>
+					<td><c:out value="${c.mail}"/></td>
+					<td><c:out value="${c.comment}"/></td>
+				</tr>
+			</c:forEach>
+		</table>
+		</br>
+		<h3>Deje su comentario aquí</h3>
+		<form role="form" action="viewHotel" method="post">
+		<div>
+			<input type="hidden" name="code" value="${hotel.code}"/>
+			<div class="form-group"><label>Comentario </label><textarea name="comentario" class="form-control" placeholder="Comentario"></textarea></div>
+			<button type="submit" class="btn btn-primary">Enviar</button>
+			<button type="reset" class="btn btn-default">Limpiar</button>
+		</div>
+		</form>
+	</div>
+	</body>
+</html>
